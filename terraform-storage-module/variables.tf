29ce2e92b11ad841ba317af7a1df04a07e9a87ac
Michael Hawthorne <mph005@gmail.com>
2025-02-11 14:27:18 -0600

    Testing

--- terraform-storage-module/variables.tf
+++ terraform-storage-module/variables.tf
@@ -19,21 +19,6 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "storage_account_name" {
-  description = "The name of the storage account."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-}
-
-variable "location" {
-  description = "The location of the resource group."
-  type        = string
-}
-
 variable "account_tier" {
   description = "The tier of the storage account."
   type        = string
@@ -47,4 +32,70 @@ variable "account_replication_type" {
 variable "tags" {
   description = "A map of tags to assign to the resource."
   type        = map(string)
+}
+
+variable "os_disk_name" {
+  description = "The name of the OS disk."
+  type        = string
+}
+
+variable "virtual_machine_name" {
+  description = "The name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type        = string
+}
+
+variable "os_disk_caching" {
+  description = "The caching type for the OS disk"
+  type        = string
+}
+
+variable "os_disk_create_option" {
+  description = "The create option for the OS disk"
+  type        = string
+}
+
+variable "os_disk_managed_disk_type" {
+  description = "The managed disk type for the OS disk"
+  type        = string
+}
+
+variable "image_publisher" {
+  description = "The publisher of the VM image"
+  type        = string
+}
+
+variable "image_offer" {
+  description = "The offer of the VM image"
+  type        = string
+}
+
+variable "image_sku" {
+  description = "The SKU of the VM image"
+  type        = string
+}
+
+variable "image_version" {
+  description = "The version of the VM image"
+  type        = string
+}
+
+variable "computer_name" {
+  description = "The computer name of the VM"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "The admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "The admin password for the VM"
+  type        = string
+  sensitive   = true
 }
